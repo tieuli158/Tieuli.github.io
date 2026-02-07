16ca6dcc8f06243817a6a5c1e14939da3e731910
tieuli158 <tieuli158@gmail.com>
2026-02-07 19:23:33 +0700

    Update status.js

--- status.js
+++ status.js
@@ -1,7 +1,7 @@
 // File này dùng để bật tắt tool từ xa
 // 1 = Hoạt động bình thường
 // 0 = Bảo trì (Khóa tool)
-var SYSTEM_STATUS = 0; 
+var SYSTEM_STATUS = 1; 
 
 // Lời nhắn bảo trì
 var MSG_BAOTRI = "Tool đang cập nhật phiên bản mới.\nVui lòng quay lại sau 30 phút!";
